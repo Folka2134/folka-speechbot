8bc67bd2cee4e94c0a82b5368ce63784b55b5c62
Folka2134 <folka2134@gmail.com>
2024-01-10 10:47:16 +0000

    backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default {
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Your Express server address
+        target: 'http://localhost:5173', // Your Express server address
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
